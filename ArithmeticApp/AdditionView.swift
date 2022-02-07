d47a978b82997e000c9b63ef4193fd80209cf5b7
Judy <jyu@lcs.on.ca>
2022-02-07 18:18:31 -0500

    Create an addition view

---
+++ ArithmeticApp/AdditionView.swift
@@ -0,0 +1,20 @@
+//
+//  AdditionView.swift
+//  ArithmeticApp
+//
+//  Created by Judy Yu on 2022-02-07.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdditionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdditionView()
+    }
+}
