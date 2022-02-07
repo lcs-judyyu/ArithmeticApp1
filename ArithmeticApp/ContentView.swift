29afe5c6357e61b0b3d6c47fcae3ee1fde2c23c7
Judy <jyu@lcs.on.ca>
2022-02-07 09:25:16 -0500

    Have part of a static layout done

--- ArithmeticApp/ContentView.swift
+++ ArithmeticApp/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center, spacing: 60) {
+            HStack(alignment: .center, spacing: 30) {
+                Text("✕")
+                    .font(.system(size: 52))
+                Spacer()
+                VStack(alignment: .trailing, spacing: 0) {
+                    Text("5")
+                    Text("6")
+                }
+                .padding(20)
+            }
+            .font(.system(size: 72))
+            Divider()
+            Text("30")
+                .font(.system(size: 72))
+            Button(action: {
+                print("Button was pressed")
+            }, label: {
+                Text("Check Answer")
+            })
+                .buttonStyle(.bordered)
+            Spacer()
+        }
+        .padding(60)
     }
 }
 
