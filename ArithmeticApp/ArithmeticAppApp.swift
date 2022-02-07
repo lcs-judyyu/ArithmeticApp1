1cdafc20174dad0120d562afdfc2ef5e70f69872
Judy <jyu@lcs.on.ca>
2022-02-07 08:52:11 -0500

    Initial Commit

---
+++ ArithmeticApp/ArithmeticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticAppApp.swift
+//  ArithmeticApp
+//
+//  Created by Judy Yu on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
