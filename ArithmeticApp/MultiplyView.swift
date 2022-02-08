5109d86442fa21b00822d4a5d1fb836c40bb1338
Judy <jyu@lcs.on.ca>
2022-02-08 11:51:48 -0500

    Make the check answer button disappear and replaced with the new question button when user checks the answer

--- ArithmeticApp/MultiplyView.swift
+++ ArithmeticApp/MultiplyView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct MultiplyView: View {
     
     // MARK: Stored properties
-    let multiplicand = Int.random(in: 1...12)
-    let multiplier = Int.random(in: 1...12)
+    //@State updates user interface when the values are changed
+    @State var multiplicand = Int.random(in: 1...12)
+    @State var multiplier = Int.random(in: 1...12)
+    //this string contains user input
     @State var inputGiven = ""
     
     // Has an answer been checked?
@@ -19,6 +21,7 @@ struct MultiplyView: View {
     
     // Was the answer given actually correct?
     @State var answerCorrect = false
+    @State var answerFalse = false
     
     // MARK: Computed properties
     // What is the correct product?
@@ -27,7 +30,7 @@ struct MultiplyView: View {
     }
     
     var body: some View {
-                
+        
         VStack(spacing: 0) {
             HStack {
                 Text("✕")
@@ -43,10 +46,19 @@ struct MultiplyView: View {
             Divider()
             
             HStack {
-                Image(systemName: "checkmark.circle")
-                    .foregroundColor(.green)
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(Color.green)
                     //        CONDITION      true  false
-                    .opacity(answerCorrect ? 1.0 : 0.0)
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                    Image(systemName: "xmark.circle")
+                        .foregroundColor(Color.red)
+                    //        CONDITION      true  false
+                        .opacity(answerFalse ? 1.0 : 0.0)
+                    // or
+                    //  .opacity(answerChecked && answerCorrect == false ? 1.0 : 0.0)
+                }
+                
                 Spacer()
                 TextField("Enter Product",
                           text: $inputGiven)
@@ -54,39 +66,62 @@ struct MultiplyView: View {
                     .multilineTextAlignment(.trailing)
             }
             
-            Button(action: {
-                
-                // Answer has been checked!
-                answerChecked = true
-                
-                // Convert the input given to an integer, if possible
-                guard let productGiven = Int(inputGiven) else {
-                    // Sadness, not a number
-                    answerCorrect = false
-                    return
-                }
-
-                // Check the answer!
-                if productGiven == correctProduct {
-                    // Celebrate! 👍🏼
-                    answerCorrect = true
+            ZStack {
+                if  answerChecked == false {
+                    Button(action: {
+                        
+                        // Answer has been checked!
+                        answerChecked = true
+                        
+                        // Convert the input given to an integer, if possible
+                        guard let productGiven = Int(inputGiven) else {
+                            // Sadness, not a number
+                            answerCorrect = false
+                            answerFalse = true
+                            return
+                        }
+                        
+                        // Check the answer!
+                        if productGiven == correctProduct {
+                            // Celebrate! 👍🏼
+                            answerCorrect = true
+                            answerFalse = false
+                        } else {
+                            // Sadness, they gave a number, but it's correct 😭
+                            answerCorrect = false
+                            answerFalse = true
+                        }
+                    }, label: {
+                        Text("Check Answer")
+                            .font(.title)
+                    })
+                        .padding(10)
+                        .buttonStyle(.bordered)
                 } else {
-                    // Sadness, they gave a number, but it's correct 😭
-                    answerCorrect = false
+                    
+                    Button(action: {
+                        answerChecked = false
+                        answerCorrect = false
+                        answerFalse = false
+                        multiplicand = Int.random(in: 1...12)
+                        multiplier = Int.random(in: 1...12)
+                        inputGiven = ""
+                    }, label: {
+                        Text("New Question")
+                            .font(.title)
+                    })
+                        .padding(10)
+                        .buttonStyle(.bordered)
                 }
-            }, label: {
-                Text("Check Answer")
-                    .font(.largeTitle)
-            })
-                .padding()
-                .buttonStyle(.bordered)
+            }
+            
             
             Spacer()
         }
-        .padding(.horizontal, 30)
+        .padding(.horizontal, 50)
         .padding(.vertical, 50)
         .font(.system(size: 72))
-
+        
         
     }
 }
